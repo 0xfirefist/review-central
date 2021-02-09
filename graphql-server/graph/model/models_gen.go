7af5e8f8e4ddc6a40070ba28f12b16d6bed8fe5e
Dev Kalra <kalradev@outlook.com>
2021-02-09 14:06:56 +0530

    backend login functionality added

--- graphql-server/graph/model/models_gen.go
+++ graphql-server/graph/model/models_gen.go
@@ -2,19 +2,20 @@
 
 package model
 
-type NewTodo struct {
-	Text   string `json:"text"`
-	UserID string `json:"userId"`
+type Login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
-type Todo struct {
-	ID   string `json:"id"`
-	Text string `json:"text"`
-	Done bool   `json:"done"`
-	User *User  `json:"user"`
+type NewUser struct {
+	FirstName  *string `json:"firstName"`
+	MiddleName *string `json:"middleName"`
+	LastName   *string `json:"lastName"`
+	Email      *string `json:"email"`
+	Password   *string `json:"password"`
+	Number     *string `json:"number"`
 }
 
-type User struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+type RefreshTokenInput struct {
+	Token string `json:"token"`
 }
---
+++ internal/users/users.go
@@ -0,0 +1,42 @@
+package users
+
+import "errors"
+
+type User struct {
+	// Username   string `json:"username"`
+	FirstName  string `json:"first-name"`
+	MiddleName string `json:"middle-name"`
+	LastName   string `json:"last-name"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	Number     string `json:"number"`
+}
+
+var users []*User
+
+// Create - add user to the database
+func (user *User) Create() {
+	users = append(users, user)
+}
+
+//GetUserByUsername check if a user exists in database by given username
+func GetUserByEmail(email string) (*User, error) {
+
+	for _, user := range users {
+		if user.Email == email {
+			return user, nil
+		}
+	}
+
+	return nil, errors.New("User not found")
+}
+
+// Authenticate user while login
+func (u *User) Authenticate() bool {
+	for _, user := range users {
+		if u.Email == user.Email && u.Password == user.Password {
+			return true
+		}
+	}
+	return false
+}
