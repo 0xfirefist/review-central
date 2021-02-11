20cba6fa16061bb38f4dbaf51a3f7455dcccd407
Dev Kalra <kalradev@outlook.com>
2021-02-11 15:08:26 +0530

    server code change

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 
 	mux "github.com/gorilla/mux"
 	server "github.com/kalradev/review-central/graphql-server"
@@ -27,13 +26,13 @@ func main() {
 	router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(uiDirectory))))
 
 	// running server
-	srv := &http.Server{
-		Handler: router,
-		Addr:    "127.0.0.1:8000",
-		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
+	// srv := &http.Server{
+	// 	Handler: router,
+	// 	Addr:    "127.0.0.1:8000",
+	// 	// Good practice: enforce timeouts for servers you create!
+	// 	WriteTimeout: 15 * time.Second,
+	// 	ReadTimeout:  15 * time.Second,
+	// }
 	log.Println("Started Listening on Port : 8000")
-	log.Fatal(srv.ListenAndServe())
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
