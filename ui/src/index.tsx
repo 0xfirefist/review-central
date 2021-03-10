d1e57e094d50ea84c7cfa802217e479f9acc09a7
Dev Kalra <kalradev@outlook.com>
2021-03-10 21:19:21 +0530

    file name change

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -5,7 +5,7 @@ import { CookiesProvider } from "react-cookie";
 import Login from './components/login'
 import Register from './components/register'
 import AddReview from './components/add-review'
-import EditReview from './components/edit_review'
+import OffsetReview from './components/offset-review'
 import Profile from './components/profile'
 import MyReviews from './components/my-reviews';
 import {HashRouter, Route, Switch} from 'react-router-dom'
@@ -27,7 +27,7 @@ ReactDOM.render(
             <HashRouter>
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
-                    <Route exact path='/edit-review' component={EditReview}/>
+                    <Route exact path='/offset-review' component={OffsetReview}/>
                     <Route exact path='/login' component={Login}/>
                     <Route exact path='/register' component={Register}/>
                     <Route exact path='/add-review' component={AddReview}/>
