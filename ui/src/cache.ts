2b62441b5e119e7513d78348d204e1ce2f54807f
Dev Kalra <kalradev@outlook.com>
2021-03-16 14:56:22 +0530

    login issue resolved

--- ui/src/cache.ts
+++ ui/src/cache.ts
@@ -1,8 +1,15 @@
 import { InMemoryCache, Reference, makeVar } from '@apollo/client';
 
+function readCookie(name) {
+    name += '=';
+    for (var ca = document.cookie.split(/;\s*/), i = ca.length - 1; i >= 0; i--)
+        if (!ca[i].indexOf(name))
+            return ca[i].replace(name, '');
+}
+
 // Initializes to true if localStorage includes a 'token' key,
 // false otherwise
-export const isLoggedInVar = makeVar<boolean>(false);
+export const isLoggedInVar = makeVar<boolean>(readCookie("user")!==undefined);
 
 // Initializes to an empty array
 // export const cartItemsVar = makeVar<string[]>([]);
